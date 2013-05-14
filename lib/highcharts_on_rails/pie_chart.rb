76d5c61ea00a6a8205dd923e4dc1769d5991793f
Stevo <b.kosmowski@selleo.com>
2013-05-14 08:25:29 +0200

    update default colors

--- lib/highcharts_on_rails/pie_chart.rb
+++ lib/highcharts_on_rails/pie_chart.rb
@@ -69,7 +69,7 @@ class HighchartsOnRails::PieChart < HighchartsOnRails::BaseChart
   private
 
   def get_colors
-    @colors ||= %w(#FF0000 #FFFFFF)
+    @colors ||= %w(#2f7ed8 #0d233a #8bbc21 #910000 #1aadce #492970 #f28f43 #77a1e5 #c42525 #a6c96a)
   end
 
   def get_color
